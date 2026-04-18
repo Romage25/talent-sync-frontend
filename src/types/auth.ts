02fce3ebb2f5e77926989f53e084d8ae5092c37c
Rodel Gerodias <rodelgerodias25@gmail.com>
2026-04-18 14:53:53 +0800

    chore: remove unnecessary styles feat: add api axios feat: add users and auth interfaces feat: add handleLaravelError utility function

---
+++ src/types/auth.ts
@@ -0,0 +1,19 @@
+export interface LoginForm {
+  email: string;
+  password: string;
+}
+
+export interface RegisterForm {
+  first_name: string;
+  last_name: string;
+  address: string;
+  phone_no: string;
+  role: '' | 'recruiter' | 'applicant';
+  email: string;
+  password: string;
+  password_confirmation: string;
+}
+
+export interface LaravelValidationErrors {
+  [key: string]: string[];
+}
